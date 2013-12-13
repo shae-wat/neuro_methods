74bfa24ad6a2a2c1202a574f7de4002d5faf6d31
Shaelyn Watson <shaelynjoy@gmail.com>
2013-12-13 12:29:32 -0600

    updated delta fxn

--- f_xcal.m
+++ f_xcal.m
@@ -1,24 +1,16 @@
 function [dw] = f_xcal(plus_phase_activation, minus_phase_activation, activated_motor_unit, rev_di)
 
 
-
 theta_d = rev_di;
 
 %theta_p = dynamic_thres_param;
 
 
-
 a = plus_phase_activation*activated_motor_unit';%plusphase
-
 b = minus_phase_activation*activated_motor_unit';%minus phas
 
 
-
-
-
 dw = zeros(size(a));
-
-
 
 ps = a>(b.*theta_d);
 
@@ -26,5 +18,5 @@ dw = a-b;
 
 dw(ps) = a(ps)-b(ps);
 
- dw(~ps) = -b(~ps)*(1-theta_d)/theta_d;
+dw(~ps) = -b(~ps)*(1-theta_d)/theta_d;
 
